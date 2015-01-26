8e4fc205e3e19236123e1dae0e8ddacd7ef11a91
VanPalamar <rami.morri@gmail.com>
2015-01-26 16:18:58 +0100

    Our famous Car

---
+++ LiveItLibrary/Enums/ECarTexture.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ECarTexture.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The e car texture.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LiveIT2._1.Enums
+{
+    /// <summary>
+    /// The e car texture.
+    /// </summary>
+    public enum ECarTexture
+    {
+        /// <summary>
+        /// The main player car.
+        /// </summary>
+        MainPlayerCar,
+
+        /// <summary>
+        /// The alternate player car.
+        /// </summary>
+        AlternatePlayerCar,
+
+        /// <summary>
+        /// The tank.
+        /// </summary>
+        Tank
+    }
+}
